1a559f8876ec5c45a1bd86fa0943648e2f850b8f
dancingalfred <robban021@hotmail.com>
2022-11-03 11:06:45 +0100

    fixed functions and added a test notebook

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import datetime
 import random
+import pandas as pd
 
 
 def createListofDays(startDate:datetime, nrOfDays:int):
@@ -9,17 +10,39 @@ def createListofDays(startDate:datetime, nrOfDays:int):
     result = []
 
     while len(result) < nrOfDays:
-        result.append(dt.strftime('%Y-%m-%d %H:%M:%S'))
+        result.append(dt)
         dt += step
     
     return result
 
 def generateDays(listWithDates):
 
-    listOfDaysWithValues =[]
+    listOfTimeWithValues =[]
+    indoorTemp = 23.1
+    inbdoorHumidity = 26.0  
 
     for day in listWithDates:
-        date = listWithDates[day]
+        date = day
+        for hour in range(0,23):
+            date = date + datetime.timedelta(hours=1)
+            if indoorTemp < 19.8:
+                indoorTemp = indoorTemp + 0.1
+            elif indoorTemp > 29.4:
+                indoorTemp = indoorTemp - 0.1
+            else: 
+                indoorTemp = indoorTemp + random.uniform(-0.3, 0.3)
 
+            if inbdoorHumidity < 21.1:
+                inbdoorHumidity = inbdoorHumidity + 0.1
+            elif inbdoorHumidity > 49.5:
+                inbdoorHumidity = inbdoorHumidity - 0.1
+            else: 
+                inbdoorHumidity = inbdoorHumidity + random.uniform(-0.5, 0.5)
 
-    pass
+            hourlyReading = [date,indoorTemp,inbdoorHumidity]
+            listOfTimeWithValues.append(hourlyReading)
+    return listOfTimeWithValues        
+
+def convertToDf(list:list):
+    df = pd.DataFrame(list, columns = ['Time', 'Indoor Temperature', 'Indoor Temperature'])
+    return df
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import random
 import datetime
-from functions import createListofDays, generateDays
+from functions import createListofDays, generateDays, convertToDf
 
 startDate = datetime.datetime(2020, 1, 1)
 listOfDays = createListofDays(startDate,365)
 
 listOfDaysWithValues = generateDays(listOfDays)
+dataFrame = convertToDf(listOfDaysWithValues)
 
 
 
